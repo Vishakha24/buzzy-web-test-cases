4e988c47e48e316a489bc839e618d06adcc76da8
Vishakha24 <vishakha.chaudhary@twinspark.co>
2017-03-16 12:37:51 +0530

    test cases for api's of rewardApis

--- tests/_support/ApiRewardsTester.php
+++ tests/_support/ApiRewardsTester.php
@@ -25,12 +25,61 @@ class ApiRewardsTester extends \Codeception\Actor
     * Define custom actions here
     */
 
+    public function login($username,$password)
+    {
+       $I = $this;
+       $I->wantTo('Login to super admin dashboard');
+       $I->amOnPage('staffs/login');
+       $I->fillField('#username', $username);
+       $I->fillField('#password', $password);
+       $I->click('Login');
+    }
+
+    public function logout()
+    {
+        $I = $this;
+
+        $I->click(Locator::find('a', ['class' => 'fa fa-sign-out']));
+        $I->wait(3);
+
+    }
+
+   // api to add reseller program
+
+   public function addResellerProgram($name, $creditType, $featureId1, $featureId2, $featureId3){
+        $I = $this;
+        $I->amGoingTo("add new program");
+        $I->haveHttpHeader('Accept', 'application/json'); 
+        $I->haveHttpHeader('Content-Type','application/json');
+        $I->sendPOST('reseller/resellerPrograms/addResellerProgram', json_encode(['program_name' => $name, 'credit_type' => $creditType, 'features' => array([$featureId1, $featureId2, $featureId3])]));
+        $I->seeResponseCodeIs(200);
+        $response = $I->grabResponse();
+        // $programId = $I->grabDataFromResponseByJsonPath('$.response.id');
+        // codecept_debug($programId);
+
+   }
+
+   public function generateToken($programId, $clientId, $clientSecret){
+        $I = $this;
+        $programId = $programId[0];
+        $token = base64_encode($clientId.':'. $clientSecret);
+        $I->amGoingTo('generate token for reseller program');
+        $I->haveHttpHeader('Authorization ','Basic '.$token);
+        $I->haveHttpHeader('Accept', 'application/json'); 
+        $I->haveHttpHeader('Content-Type','application/json');
+        $I->sendPOST('api/resellerPrograms/token', json_encode(['reseller_program_id' => $programId]));
+        $I->seeResponseCodeIs(200);
+        $response = $I->grabResponse();
+        $I->assertNotEmpty('$response','Response contains data');
+        $reseller_token = $I->grabDataFromResponseByJsonPath('$.data.token');
+   } 
+
    public function add_patient_user($reseller_token, $name, $email, $phone)
    {
         $reseller_token = $reseller_token[0];
         $I = $this;
         $I->amGoingTo("add new patient");
-        $I->haveHttpHeader('token ','Bearer '.$reseller_token);
+        $I->haveHttpHeader('Authorization ','Bearer '.$reseller_token);
         $I->haveHttpHeader('Accept', 'application/json'); 
         $I->haveHttpHeader('Content-Type','application/json');
         $I->sendPOST('api/users', json_encode(['name' => $name, 'email'=>$email, 'phone'=>$phone]));
@@ -38,15 +87,41 @@ class ApiRewardsTester extends \Codeception\Actor
         $user = $I->grabResponse();
    }
 
+   public function edit_patient_user($reseller_token, $name, $id)
+   {    
+        $reseller_token = $reseller_token[0];
+        $I = $this;
+        $I->amGoingTo("edit patient");
+        $I->haveHttpHeader('Authorization ','Bearer '.$reseller_token);
+        $I->haveHttpHeader('Accept', 'application/json'); 
+        $I->haveHttpHeader('Content-Type','application/json');
+        $I->sendPUT('api/users/edit/'.$id, json_encode(['name' => $name]));
+        $I->seeResponseCodeIs(200);
+        $user = $I->grabResponse();
+   }
+
    public function add_tier($reseller_token,$tiername, $lowerbound, $upperbound, $multiplier)
    {
         $reseller_token = $reseller_token[0];
-       	$I = $this;
-       	$I->amGoingTo("add new tier");
-        $I->haveHttpHeader('token ','Bearer '.$reseller_token);
+        $I = $this;
+        $I->amGoingTo("add new tier");
+        $I->haveHttpHeader('Authorization ','Bearer '.$reseller_token);
         $I->haveHttpHeader('Accept', 'application/json'); 
         $I->haveHttpHeader('Content-Type','application/json');
-        $I->sendPOST('api/tiers', json_encode(['name' => $tiername,'lowerbound'=>$lowerbound, 'upperbound'=>$upperbound, 'multiplier'=>$multiplier]));
+         $I->sendPOST('api/tiers', json_encode(['name' => $tiername,'lowerbound'=>$lowerbound, 'upperbound'=>$upperbound, 'multiplier'=>$multiplier]));
+        $I->seeResponseCodeIs(200);
+        $tier = $I->grabResponse();
+   }
+
+   public function edit_tier($reseller_token,$tiername, $id)
+   {
+        $reseller_token = $reseller_token[0];
+        $I = $this;
+        $I->amGoingTo("edit tier");
+        $I->haveHttpHeader('Authorization ','Bearer '.$reseller_token);
+        $I->haveHttpHeader('Accept', 'application/json'); 
+        $I->haveHttpHeader('Content-Type','application/json');
+         $I->sendPUT('api/tiers/edit/'.$id, json_encode(['name' => $tiername]));
         $I->seeResponseCodeIs(200);
         $tier = $I->grabResponse();
    }
@@ -60,7 +135,18 @@ class ApiRewardsTester extends \Codeception\Actor
         $I->haveHttpHeader('Content-Type','application/json');
         $I->sendPOST('api/tierPerks', json_encode(['tier_id' => $tierId, 'perk' => $perk]));
         $I->seeResponseCodeIs(200);
-        $tier = $I->grabResponse();
+        $tierPerk = $I->grabResponse();
+   }
+
+    public function edit_tier_perks($perk, $id)
+   {
+        $I = $this;
+        $I->amGoingTo("edit tier perk");
+        $I->haveHttpHeader('Accept', 'application/json'); 
+        $I->haveHttpHeader('Content-Type','application/json');
+        $I->sendPUT('api/tierPerks/edit/'.$id, json_encode(['perk' => $perk]));
+        $I->seeResponseCodeIs(200);
+        $tierPerk = $I->grabResponse();
    }
 
    public function add_promotion($reseller_token,$promoname, $description, $points, $frequency)
@@ -68,23 +154,37 @@ class ApiRewardsTester extends \Codeception\Actor
         $reseller_token = $reseller_token[0];
         $I = $this;
         $I->amGoingTo("add new promotion");
-        $I->haveHttpHeader('token ','Bearer '.$reseller_token);
+        $I->haveHttpHeader('Authorization ','Bearer '.$reseller_token);
         $I->haveHttpHeader('Accept', 'application/json'); 
         $I->haveHttpHeader('Content-Type','application/json');
         $I->sendPOST('api/promotions', json_encode(['name' => $promoname,'description'=>$description, 'points'=>$points, 'frequency'=>$frequency]));
         $I->seeResponseCodeIs(200);
         $tier = $I->grabResponse();
    }
-   public function promotion_awards($reseller_token, $user, $promoId1, $promoId2, $promotion)
+
+   public function edit_promotion($reseller_token,$promoname, $frequency, $id)
+   {
+        $reseller_token = $reseller_token[0];
+        $I = $this;
+        $I->amGoingTo("edit promotion");
+        $I->haveHttpHeader('Authorization ','Bearer '.$reseller_token);
+        $I->haveHttpHeader('Accept', 'application/json'); 
+        $I->haveHttpHeader('Content-Type','application/json');
+        $I->sendPUT('api/promotions/edit/'.$id, json_encode(['name' => $promoname,'frequency'=>$frequency]));
+        $I->seeResponseCodeIs(200);
+        $tier = $I->grabResponse();
+   }
+   public function promotion_awards($reseller_token, $user, $promotion)
    {
       $reseller_token = $reseller_token[0];
       $user = $user[0];
       $I = $this;
       $I->amGoingTo("award promotion to user");
-      $I->haveHttpHeader('token ','Bearer '.$reseller_token);
-      $I->haveHttpHeader('Accept', 'application/json'); 
-      $I->haveHttpHeader('Content-Type','application/json');
-      $I->sendPOST('api/awards/promotions', json_encode(["selectedPromotions" => array($promoId1, $promoId2, $promotion), 'user_id' => $user]));
+      $I->haveHttpHeader('Authorization ','Bearer '.$reseller_token);
+        $I->haveHttpHeader('Accept', 'application/json'); 
+        $I->haveHttpHeader('Content-Type','application/json');
+      // $I->sendPOST('api/awards/promotions', json_encode(["selectedPromotions" => array([$promoId1, $promotion]), 'user_id' => $user]));
+      $I->sendPOST('api/awards/promotions', json_encode(['user_id' => $user, "selectedPromotions" => array($promotion)]));
       $I->seeResponseCodeIs(200);
       $promotionaward = $I->grabResponse();
    }
@@ -95,7 +195,7 @@ class ApiRewardsTester extends \Codeception\Actor
       $user = $user[0];
       $I = $this;
       $I->amGoingTo("award tier to user");
-      $I->haveHttpHeader('token ','Bearer '.$reseller_token);
+      $I->haveHttpHeader('Authorization','Bearer '.$reseller_token);
       $I->haveHttpHeader('Accept', 'application/json'); 
       $I->haveHttpHeader('Content-Type','application/json');
       $I->sendPOST('api/awards/tier', json_encode(['user_id' => $user, 'amount' => $amount, 'bountee_transaction_id' => $bounteeId]));
@@ -103,10 +203,12 @@ class ApiRewardsTester extends \Codeception\Actor
       $tieraward = $I->grabResponse();
    }
 
+   // doubt 
    public function add_surveys($survey_name)
    {
         $I = $this;
         $I->amGoingTo("add new survey");
+        // $I->haveHttpHeader('Authorization ','Bearer '.$reseller_token);
         $I->haveHttpHeader('Accept', 'application/json'); 
         $I->haveHttpHeader('Content-Type','application/json');
         $I->sendPOST('api/surveys/add', json_encode(['name' => $survey_name]));
@@ -118,8 +220,8 @@ class ApiRewardsTester extends \Codeception\Actor
    {
         $I = $this;
         $I->amGoingTo("add new question");
-        $I->haveHttpHeader('Accept', 'application/json'); 
-        $I->haveHttpHeader('Content-Type','application/json');
+        $I->haveHttpHeader('Accept', 'application/json', 'Content-Type','application/json'); 
+        // $I->haveHttpHeader('Content-Type','application/json');
         $I->sendPOST('api/surveys/add_questions', json_encode(['question_type_id' => $question_type_id, 'text' => $text, 'frequency' => $frequency, 'points' => $points]));
         $I->seeResponseCodeIs(200);
         $question = $I->grabResponse();
@@ -136,15 +238,15 @@ class ApiRewardsTester extends \Codeception\Actor
         $surveyQuestion = $I->grabResponse();
    }
 
-   public function add_reseller_program_survey($reseller_token, $surveyId)
+   public function add_reseller_program_survey($reseller_token, $surveyId, $surveyType)
    {
         $reseller_token = $reseller_token[0];
         $I = $this;
         $I->amGoingTo("add reseller program survey");
-        $I->haveHttpHeader('token ','Bearer '.$reseller_token);
+        $I->haveHttpHeader('Authorization ','Bearer '.$reseller_token);
         $I->haveHttpHeader('Accept', 'application/json'); 
         $I->haveHttpHeader('Content-Type','application/json');
-        $I->sendPOST('api/surveys/add_reseller_program_survey', json_encode(['survey_id' => $surveyId]));
+        $I->sendPOST('api/surveys/add_reseller_program_survey', json_encode(['survey_id' => $surveyId, 'survey_type' => $surveyType]));
         $I->seeResponseCodeIs(200);
         $resellerProgramSurvey = $I->grabResponse();
    }
@@ -159,4 +261,84 @@ class ApiRewardsTester extends \Codeception\Actor
         $I->seeResponseCodeIs(200);
         $resellerProgramSurveyQuestion = $I->grabResponse();
    }
+
+   public function addMilestone($reseller_token, $name, $is_limited, $duration, $level_name, $level_number, $rewardtype, $points, $level_rule)
+   {
+        $reseller_token = $reseller_token[0];
+        $I = $this;
+        $I->amGoingTo("add milestone");
+        $I->haveHttpHeader('Authorization ','Bearer '.$reseller_token);
+        $I->haveHttpHeader('Accept', 'application/json'); 
+        $I->haveHttpHeader('Content-Type','application/json');
+        $I->sendPOST('api/resellerProgramMilestones/addmilestone', json_encode(['name' => $name, 'is_limited' => $is_limited, 'duration' => $duration, 'milestone_levels' => array(array('name' => $level_name, 'level_number' => $level_number, 'milestone_level_rewards'=> array('reward_type_id' => $rewardtype, 'points' => $points), 'milestone_level_rules' => array('level_rule' => $level_rule)))]));
+        $I->seeResponseCodeIs(200);
+        $milestone = $I->grabResponse();
+   }
+
+    public function surveyAndMilestoneAward($reseller_token, $user, $resellerProgSurveyQuesId, $response, $perfectSuvey, $resellerProgSurveyQuesId1, $response1, $perfectSuvey1)
+    {
+        $reseller_token = $reseller_token[0];
+        $user = $user[0];
+        $I = $this;
+        $I->amGoingTo("award tier to user");
+        $I->haveHttpHeader('Authorization ','Bearer '.$reseller_token);
+        $I->haveHttpHeader('Accept', 'application/json'); 
+        $I->haveHttpHeader('Content-Type','application/json');
+        $I->sendPOST('api/surveyAwards/saveResponses', json_encode(['user_id' => $user, 'survey_instance_responses' => array(['reseller_program_survey_question_id' => $resellerProgSurveyQuesId, 'response' => $response, 'forPerfectSurvey' => $perfectSuvey], ['reseller_program_survey_question_id' => $resellerProgSurveyQuesId1, 'response' => $response1, 'forPerfectSurvey' => $perfectSuvey1])]));
+        $I->seeResponseCodeIs(200);
+        $award = $I->grabResponse();
+    }
+
+    public function add_gift_coupon($reseller_token, $points, $description, $expiryTime, $reason)
+   {
+        $reseller_token = $reseller_token[0];
+        $I = $this;
+        $I->amGoingTo("add gift coupon");
+        $I->haveHttpHeader('Authorization ','Bearer '.$reseller_token);
+        $I->haveHttpHeader('Accept', 'application/json'); 
+        $I->haveHttpHeader('Content-Type','application/json');
+        $I->sendPOST('api/giftCoupons', json_encode(['points' => $points, 'description' => $description, 'expiry_duration' => $expiryTime, 'reason' => $reason]));
+        $I->seeResponseCodeIs(200);
+        $giftCoupon = $I->grabResponse();
+   }
+
+   public function edit_gift_coupon($reseller_token, $points, $id)
+   {    
+        $reseller_token = $reseller_token[0];
+        $I = $this;
+        $I->amGoingTo("add gift coupon");
+         $I->haveHttpHeader('Authorization ','Bearer '.$reseller_token);
+        $I->haveHttpHeader('Accept', 'application/json'); 
+        $I->haveHttpHeader('Content-Type','application/json');
+        $I->sendPUT('api/giftCoupons/edit/'.$id, json_encode(['points' => $points]));
+        $I->seeResponseCodeIs(200);
+        $giftCoupon = $I->grabResponse();
+   }
+
+   public function manual_awards($reseller_token, $user, $points){
+      $reseller_token = $reseller_token[0];
+      $user = $user[0];
+      $I = $this;
+      $I->amGoingTo("manual award to user");
+      $I->haveHttpHeader('Authorization ','Bearer '.$reseller_token);
+      $I->haveHttpHeader('Accept', 'application/json'); 
+      $I->haveHttpHeader('Content-Type','application/json');
+      $I->sendPOST('api/awards/manual', json_encode(['user_id' => $user, 'points' => $points]));
+      $I->seeResponseCodeIs(200);
+      $tieraward = $I->grabResponse();
+   }
+
+   public function giftCouponAward($reseller_token, $user, $giftCouponId){
+      $reseller_token = $reseller_token[0];
+      $user = $user[0];
+      $I = $this;
+      $I->amGoingTo("award survey to user");
+      $I->haveHttpHeader('Authorization ','Bearer '.$reseller_token);
+      $I->haveHttpHeader('Accept', 'application/json'); 
+      $I->haveHttpHeader('Content-Type','application/json');
+      $I->sendPOST('api/awards/giftCoupon', json_encode(['user_id' => $user, 'gift_coupon_id' => $giftCouponId]));
+      $I->seeResponseCodeIs(200);
+      $tieraward = $I->grabResponse();
+   }
+
 }
--- tests/apiRewards/apiRewardsCest.php
+++ tests/apiRewards/apiRewardsCest.php
@@ -18,41 +18,85 @@ class apiRewardsCest
 
     public function reward_api(ApiRewardsTester $I)
     {
+          $I->login('qwerty', '12345678');
+          
+          //add reseller program
+          $I->addResellerProgram('testProgram', 'store', 1, 2, 3);
+          $programId = $I->grabDataFromResponseByJsonPath('$.response.id');
+          codecept_debug($programId);
 
-         //reseller program token
-          $resellerProgramId = 2;
-          $I->amGoingTo('generate token for reseller program');
-          $I->haveHttpHeader('token ','Basic MTIzNDU2Nzg5OjEyMzQ1Njc4OQ==');
-          $I->haveHttpHeader('Accept', 'application/json'); 
-          $I->haveHttpHeader('Content-Type','application/json');
-          $I->sendPOST('api/resellerPrograms/token', json_encode(['reseller_program_id' => $resellerProgramId]));
-          $I->seeResponseCodeIs(200);
-          $response = $I->grabResponse();
-          $I->assertNotEmpty('$response','Response contains data');
+          // generate reseller program token
+          $I->generateToken($programId, '123456789', '123456789');
           $reseller_token = $I->grabDataFromResponseByJsonPath('$.data.token');
+          codecept_debug($reseller_token);
 
-          // add new user(patient)
-          $I->add_patient_user($reseller_token, "asv", "asv@gmail.co", "9876543456");
+          // add new user or patient
+          $I->add_patient_user($reseller_token, "vckz", "vckz@gmail.co", "9876543456");
           $user = $I->grabDataFromResponseByJsonPath('$.response.data.id');
           codecept_debug($user);
-          //add tier and tier perks
-          $I->add_tier($reseller_token,'test', '100', '200', '0.01');
 
-          $I->add_tier_perks(1, "testtierperk");
+          // edit patient
+          $I->edit_patient_user($reseller_token, 'akdrm', 1);
 
+          // add promotion
           $I->add_promotion($reseller_token, "testpromo", "testpromodesc", 40, 8);
           $promotion = $I->grabDataFromResponseByJsonPath('$.response.data.id');
           codecept_debug($promotion);
+          $promotion = $promotion[0];
 
+          // award promotion
+          $I->promotion_awards($reseller_token, $user, $promotion);
+          // edit promotion
+          $I->edit_promotion($reseller_token,'promo1', 5, 1);
+          // $promotion = $I->grabDataFromResponseByJsonPath('$.promotion.id');
+          // codecept_debug($promotion);
+          // $promotion = $promotion[0];
+ 
+
+
+          //add tier
+          $I->add_tier($reseller_token,'test tier', '100', '200', '0.01');
+
+          // edit tier
+          $I->edit_tier($reseller_token,'tier 1', 1);
+
+          // add tier perks
+          $I->add_tier_perks(1, "testtierperk");
+
+          // edit tier perk
+          $I->edit_tier_perks('perk 1', 1);
+
+          // award tiers
           $I->tier_awards($reseller_token, $user, '1000', 1);
 
-          $I->promotion_awards($reseller_token, $user, 3, 4, $promotion);
+          //add gift coupon
+          $I->add_gift_coupon($reseller_token, 100, 'gift coupon', 8, 'gift coupon');
 
-          $I->add_surveys("survey test");
-          $I->add_questions(1, "testing", 7, 50);
-          $I->add_survey_questions(1, 1, 2, 2);
-          $I->add_reseller_program_survey($reseller_token, 1);
+          //edit gift coupon
+          $I->edit_gift_coupon($reseller_token, 250, 1);
+
+          //manual award
+          $I->manual_awards($reseller_token, $user, 100);
+
+          // //gift coupon award(api is not working)
+          // $I->giftCouponAward($reseller_token, $user, 1);
+
+          // // api is working fine but there is prob in test case
+          // $I->add_surveys("survey test");
+          // $I->add_questions(1, "testing", 7, 50);
+          // $I->add_survey_questions(1, 1, 2, 2);
+
+          // add reseller program survey 
+          $I->add_reseller_program_survey($reseller_token, 1, 'perfect');
+
+          // add reseller program survey question
           $I->add_reseller_program_survey_question(1, 1, 50);
+
+          // add milestone program 
+          $I->addMilestone($reseller_token, 'milestone program', 1, 10, 'level 1', 1, 1, 100, 3);
+
+          //survey and milestone award
+          $I->surveyAndMilestoneAward($reseller_token, $user, 1, 'yes', 1, 2, 'no', 0);
     }
 
 }
